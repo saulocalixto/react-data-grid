37c4de15db10d890e1ef4124d9b911b3d565bce7
Lucas Sampaio Dias <lucassampaiodias@gmail.com>
2018-10-19 17:43:50 -0300

    Add WarcraftAPI.js

---
+++ packages/react-data-grid/src/utils/WarcraftAPI.js
@@ -0,0 +1,14 @@
+const key = 'awydkuy9tx3zksd7kkh2g5xchnwv4qy3';
+const headers = { 'Authorization': 'Saulo'};
+
+function getAPI(region) {
+  return 'https://' + region + '.api.battle.net/wow';
+}
+
+export const getToon = (region, realmName, toonName) =>
+  fetch(`${getAPI(region)}/character/${realmName}/${toonName}?locale=pt_BR&apikey=${key}`, {
+    method: 'GET'
+  }).then(res => res.json())
+    .then(data => data);
+
+
