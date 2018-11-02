e689c8afa9daf728760a4e1ef8d219aea951ec68
Saulo Calixto <sauloacalixto@gmail.com>
2018-11-02 14:06:39 -0300

    Version bump to 1.0.0 [ci skip]

--- Projeto-react-data-grid-wow/packages/react-data-grid/src/Canvas.js
+++ Projeto-react-data-grid-wow/packages/react-data-grid/src/Canvas.js
@@ -9,6 +9,7 @@ import { InteractionMasks } from './masks';
 import { getColumnScrollPosition } from './utils/canvasUtils';
 import {isFunction} from 'common/utils';
 import { EventTypes } from 'common/constants';
+import * as warcraftAPI from './utils/ApiWow';
 require('../../../themes/react-data-grid-core.css');
 
 class Canvas extends React.PureComponent {
@@ -88,7 +89,8 @@ class Canvas extends React.PureComponent {
   };
 
   state = {
-    scrollingTimeout: null
+    scrollingTimeout: null,
+    rows: []
   };
 
   rows = [];
@@ -98,7 +100,91 @@ class Canvas extends React.PureComponent {
   componentDidMount() {
     this.unsubscribeScrollToColumn = this.props.eventBus.subscribe(EventTypes.SCROLL_TO_COLUMN, this.scrollToColumn);
     this.onRows();
+    this.props.columns = [
+      { key: 'reino', name: 'Reino' },
+      { key: 'avatar', name: 'Avatar', width: 60, formatter: Formatters.ImageFormatter },
+      { key: 'nome', name: 'Nome' },
+      { key: 'classe', name: 'Classe' },
+      { key: 'spec', name: 'Especialização', width: 150 },
+      { key: 'ilvl', name: 'Item Level' },
+      { key: 'item0', name: 'Cabeça' },
+      { key: 'item1', name: 'Colar' },
+      { key: 'item2', name: 'Peitoral' },
+      { key: 'item3', name: 'Manto' },
+      { key: 'item4', name: 'Peitoral' },
+      { key: 'item5', name: 'Pulsos' },
+      { key: 'item6', name: 'Mãos' },
+      { key: 'item7', name: 'Cintura' },
+      { key: 'item8', name: 'Pernas' },
+      { key: 'item9', name: 'Pés' },
+      { key: 'item10', name: 'Anel 1' },
+      { key: 'item11', name: 'Anel 2' },
+      { key: 'item12', name: 'Berloque 1', width: 100 },
+      { key: 'item13', name: 'Berloque 2', width: 100 },
+      { key: 'item14', name: 'Arma Principal', width: 140 },
+      { key: 'item15', name: 'Arma Secundária', width: 140 }];
   }
+
+  pegaDadosPersonagens(personagens) {
+    personagens.forEach(personagem => {
+      const { reino, nome, regiao } = personagem;
+
+      warcraftAPI.getToon(regiao, reino, nome).then((resultado) => {
+        if (resultado.status !== 'nok') {
+          let ilvl = warcraftAPI.getToonIlvl(resultado);
+          let classe = warcraftAPI.getToonClass(resultado);
+          let spec = warcraftAPI.getSpecializationName(resultado);
+          let ilvlItems = warcraftAPI.getToonIlvlAllItems(resultado);
+          let thumbnail = resultado.thumbnail;
+          this.createRows(classe, spec, ilvl, ilvlItems, nome, reino, thumbnail, regiao);
+        }
+      });
+    });
+  }
+
+  createRows = (classe, spec, ilvl, ilvlItems, nome, reino, thumbnail, regiao) => {
+    let rows = this.state.rows;
+
+    // Por algum motivo o createRolls está sendo chamado sem enviar nenhum parâmetro. A condicional abaixo resolve exceções quando allIlvl é null, ou seja, não é enviado.
+    let allItemIlvl = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''];
+    if (!ilvlItems) {
+      allItemIlvl = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''];
+    }else {
+      allItemIlvl = ilvlItems;
+    }
+
+    for (let i = 1; i < 2; i++) {
+      rows.push({
+        reino: reino,
+        avatar: warcraftAPI.getToonImageURL(thumbnail, regiao),
+        nome: nome,
+        classe: classe,
+        spec: spec,
+        ilvl: ilvl,
+        item0: allItemIlvl[0],
+        item1: allItemIlvl[1],
+        item2: allItemIlvl[2],
+        item3: allItemIlvl[3],
+        item4: allItemIlvl[4],
+        item5: allItemIlvl[5],
+        item6: allItemIlvl[6],
+        item7: allItemIlvl[7],
+        item8: allItemIlvl[8],
+        item9: allItemIlvl[9],
+        item10: allItemIlvl[10],
+        item11: allItemIlvl[11],
+        item12: allItemIlvl[12],
+        item13: allItemIlvl[13],
+        item14: allItemIlvl[14],
+        item15: allItemIlvl[15]
+      });
+    }
+    this.setState({ rows });
+  };
+
+  pegaLinha = (i) => {
+    return this.state.rows[i];
+  };
 
   componentWillUnmount() {
     this._currentRowsRange = { start: 0, end: 0 };
@@ -185,14 +271,15 @@ class Canvas extends React.PureComponent {
   }
 
   getRows = (rowOverscanStartIdx, rowOverscanEndIdx) => {
+    this.pegaDadosPersonagens(this.props.rowGetter);
     this._currentRowsRange = { start: rowOverscanStartIdx, end: rowOverscanEndIdx };
-    if (Array.isArray(this.props.rowGetter)) {
-      return this.props.rowGetter.slice(rowOverscanStartIdx, rowOverscanEndIdx);
+    if (Array.isArray(this.state.rows)) {
+      return this.state.rows.slice(rowOverscanStartIdx, rowOverscanEndIdx);
     }
     let rows = [];
     let i = rowOverscanStartIdx;
     while (i < rowOverscanEndIdx) {
-      let row = this.props.rowGetter(i);
+      let row = this.pegaLinha(i);
       let subRowDetails = {};
       if (this.props.getSubRowDetails) {
         subRowDetails = this.props.getSubRowDetails(row);
