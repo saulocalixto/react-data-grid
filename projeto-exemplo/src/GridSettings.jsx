ef387754cf62bc9880d65f0b71f62bf298256dcf
Fernando Callata <fernandohre@hotmail.com>
2018-12-07 01:48:56 -0200

    configuração inacabada, falta persistir usando o exemplo de grupos

--- projeto-exemplo/src/GridSettings.jsx
+++ projeto-exemplo/src/GridSettings.jsx
@@ -9,17 +9,16 @@ import { Checkbox } from 'semantic-ui-react'
 class GridSettings extends Component {
 
     state = {
-        colunasMarcadas: this.obtenhaColunasMarcadas
+        modalOpen: false
     }
 
     handleClick = () => console.log("Click")
 
-    componentDidMount = () => {
-        let colunasMarcadas = this.obtenhaColunasMarcadas;
-        this.setState({colunasMarcadas})
-    }
+    handleOpen = () => this.setState({ modalOpen: true })
 
-    obtenhaColunasMarcadas = () => {
+    handleClose = () => this.setState({ modalOpen: false })
+
+    getMarkedColumns = () => {
         let vetor = [];
         this.props.colunas.reduce((result, value, index, array) => {
             if (index % 2 === 0) {
@@ -37,61 +36,69 @@ class GridSettings extends Component {
         console.log(vetor);
         return vetor;
     }
+ 
+    
 
     render() {
-        //console.log("Colunas marcadas: ",this.state);
+        
         return (
-            <Modal trigger={<Icon name="setting" size="big"></Icon>}>
+            <Modal trigger={<Icon name="setting" size="big"></Icon>} 
+                >
+
                 <Modal.Header>Configuração da visibilidade de campos da tabela</Modal.Header>
-                <Modal.Content image>
-                {/*<Image wrapped size='small' src='http://s1.1zoom.me/big0/867/421812-Kycb.jpg' />*/}
-                <Modal.Description>
-                <Table celled>
-                    <Table.Header>
-                        <Table.Row>
-                            
-                        </Table.Row>
-                    </Table.Header>
-        
-                    <Table.Body>
-                        {
-                            this.props.colunas.reduce((result, value, index, array) => {
-                                
-                                if (index % 2 === 0) {
-                                    result.push(array.slice(index, index + 2));
-                                }
-                                //console.log(result);
-                                return  result;
-                              }, []).map((par) => (
-                                
+                <Modal.Content>
+                
+                    <Modal.Description>
+                        <Table celled>
+                            <Table.Header>
                                 <Table.Row>
-                                    {
-                                        par.map(x => (
-                                            <Table.Cell>
-                                                <Checkbox key={x.chave} 
-                                                        label={x.label} 
-                                                        defaultChecked={x.visivel}/>
-                                            </Table.Cell>
-                                        ))   
-                                    }
+                                    
                                 </Table.Row>
-                            ))
-                        }
-                    </Table.Body>
-        
-                    <Table.Footer>
-                        <Table.Row>
-                        <Table.HeaderCell colSpan='3'>
-                            <Button color="green" 
-                                    content="Salvar"
-                                    />
-                            <Button color="red" 
-                                    content="Cancelar"/>
-                        </Table.HeaderCell>
-                        </Table.Row>
-                    </Table.Footer>
-                </Table>
-                </Modal.Description>
+                            </Table.Header>
+                
+                            <Table.Body>
+                                {
+                                    this.props.colunas.reduce((result, value, index, array) => {
+                                        
+                                        if (index % 2 === 0) {
+                                            result.push(array.slice(index, index + 2));
+                                        }
+                                        //console.log(result);
+                                        return  result;
+                                    }, []).map((par) => (
+                                        
+                                        <Table.Row>
+                                            {
+                                                par.map(x => (
+                                                    <Table.Cell>
+                                                        <Checkbox key={x.chave} 
+                                                                label={x.label} 
+                                                                defaultChecked={x.visivel}/>
+                                                    </Table.Cell>
+                                                ))   
+                                            }
+                                        </Table.Row>
+                                    ))
+                                }
+                            </Table.Body>
+                
+                            <Table.Footer>
+                                <Table.Row>
+                                <Table.HeaderCell colSpan='3'>
+                                    <Modal.Actions>
+                                        <Button color="green" 
+                                                content="Salvar"
+                                                positive/>
+                                        <Button color="red" 
+                                                content="Cancelar"
+                                                onClick={this.close} negative/>
+                                    </Modal.Actions>
+                                    
+                                </Table.HeaderCell>
+                                </Table.Row>
+                            </Table.Footer>
+                        </Table>
+                    </Modal.Description>
                 </Modal.Content>
             </Modal>
         );
